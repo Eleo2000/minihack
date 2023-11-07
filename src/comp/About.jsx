23d672878286979d97fefaed28a527aad570a885
Eleo <tahindrazaeleo@gmail.com>
2023-11-07 21:57:51 +0100

    mis a jour section About avec scrollTrigger

--- src/comp/About.jsx
+++ src/comp/About.jsx
@@ -10,23 +10,28 @@ const About = () => {
   useEffect(() => {
     // Animation using gsap and ScrollTrigger
     //bug a corriger xD
+    //tokony asiana end ito xD
     gsap.from('.about__img', {
       scrollTrigger: {
         trigger: '.about',
         start: 'top bottom',
+        end: 'bottom top',
         scrub: 1.9,
       },
       yPercent: 10,
-    }); 
+      width:'40%',
+    });
 
     gsap.to('.about__img', {
-        scrollTrigger: {
-          trigger: '.about',
-          start: 'top bottom',
-          scrub: 1.9,
-        },
-        yPercent: 0,
-      }); 
+      scrollTrigger: {
+        trigger: '.about',
+        start: 'top bottom',
+        end: 'bottom top',
+        scrub: 1.9,
+      },
+      yPercent: 0,
+      width:'55%',
+    });
 
     gsap.to('.about__img img', {
       scrollTrigger: {
@@ -34,7 +39,7 @@ const About = () => {
         start: 'top bottom',
         scrub: 1.9,
       },
-      scale: 1.5 ,
+      scale: 1.5,
     });
 
     gsap.to('.about__txt', {
@@ -44,6 +49,14 @@ const About = () => {
         scrub: 1.9,
       },
       yPercent: 50,
+    });
+    gsap.to('.about__txt2', {
+      scrollTrigger: {
+        trigger: '.about__wrapp',
+        start: 'top bottom',
+        scrub: 1.9,
+      },
+      yPercent: -50,
     });
   }, []);
 
@@ -57,6 +70,21 @@ const About = () => {
           />
         </div>
         <div className="about__txt">
+          <h2 className="section-title">
+            abo<span className="stroke">ut</span>
+            <span className="section-title__square"></span>
+          </h2>
+          <p className="about__p">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque esse
+            aspernatur fugit debitis quisquam. Quia exercitationem ipsum voluptas
+            voluptatum hic enim quo provident culpa possimus cupiditate! Dolorum
+            quae doloremque cum rerum ipsam inventore beatae, at odit, velit,
+            aspernatur minima! Corporis.
+          </p>
+        </div>
+
+
+        <div className="about__txt2">
           <h2 className="section-title">
             abo<span className="stroke">ut</span>
             <span className="section-title__square"></span>
